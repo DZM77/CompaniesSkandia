7b8d56b58e9053702bb12c9ca88e332d089d0960
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-04-17 10:42:25 +0200

    AddController

---
+++ Companies.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Companies.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Companies.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Company> Companies => Set<Company>();
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+    }
+}
--- Companies.API/Program.cs
+++ Companies.API/Program.cs
@@ -1,4 +1,7 @@
 
+using Companies.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Companies.API
 {
     public class Program
@@ -8,6 +11,10 @@ namespace Companies.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContext' not found.")));
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
