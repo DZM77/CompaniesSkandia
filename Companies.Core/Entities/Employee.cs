e9e232ab1a953cedfec794a13bbbdccfa4501645
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-04-17 10:20:04 +0200

    Add Entities

---
+++ Companies.Core/Entities/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Companies.Core.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Employee name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Age is a required field.")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Position is a required field.")]
+        [StringLength(maximumLength: 20, MinimumLength = 3, ErrorMessage = "Maximum length for the Position is 20 characters.")]
+        public string Position { get; set; } = string.Empty;
+        public Guid CompanyId { get; set; }
+        public Company Company { get; set; } = new Company();
+    }
+}
