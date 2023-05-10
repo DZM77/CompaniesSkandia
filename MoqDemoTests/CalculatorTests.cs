372939eef93bfd3c16d7f9a2b161a80cd10d0a60
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-05-10 10:32:23 +0200

    InlineData

---
+++ MoqDemoTests/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using MoqConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoqDemoTests
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData(1,2)]
+        [InlineData(3,4)]
+        [InlineData(5,6)]
+        public void Add_ShouldReturnSum(int nr1 , int nr2)
+        {
+            var sut = new Calculator();
+            var res = sut.Add(nr1, nr2);
+            Assert.Equal(nr1 + nr2, res);
+        }
+    }
+}
