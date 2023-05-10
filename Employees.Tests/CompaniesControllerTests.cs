d1f1649d4fcecdcccb80239a07c947868eb4a3b8
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-05-10 11:31:55 +0200

    GetCompany_IfNotAuthenticated_ShouldReturn400BadRequest2()

--- Employees.Tests/CompaniesControllerTests.cs
+++ Employees.Tests/CompaniesControllerTests.cs
@@ -5,6 +5,7 @@ using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -46,6 +47,28 @@ namespace Employees.Tests
 
             Assert.IsType<BadRequestObjectResult>(resultType);
            // Assert.Equal(400, resultType.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetCompany_IfNotAuthenticated_ShouldReturn400BadRequest2()
+        {
+            var mockClaimsPrincipal = new Mock<ClaimsPrincipal>();
+            mockClaimsPrincipal.SetupGet(c => c.Identity.IsAuthenticated).Returns(false);
+
+            var sut = new CompaniesController();
+            sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+                {
+                    User = mockClaimsPrincipal.Object
+                }
+            };
+
+            var result = await sut.GetCompany();
+            var resultType = result.Result as BadRequestObjectResult;
+
+            Assert.IsType<BadRequestObjectResult>(resultType);
+            Assert.Equal(400, resultType.StatusCode);
         }
     }
 }
