5ca8fca1f2994a944c382bdce90b0478e4ebd19f
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-05-10 10:38:03 +0200

    MemberData

--- MoqDemoTests/CalculatorTests.cs
+++ MoqDemoTests/CalculatorTests.cs
@@ -18,6 +18,43 @@ namespace MoqDemoTests
             var sut = new Calculator();
             var res = sut.Add(nr1, nr2);
             Assert.Equal(nr1 + nr2, res);
+        } 
+        
+        
+        [Theory]
+        [MemberData(nameof(GetNumbers2))]
+        public void Add_ShouldReturnSum2(int nr1 , int nr2)
+        {
+            var sut = new Calculator();
+            var res = sut.Add(nr1, nr2);
+            Assert.Equal(nr1 + nr2, res);
         }
+
+        public static IEnumerable<object[]> GetNumbers()
+        {
+            return new List<object[]>
+            {
+                new object[] { 1,2},
+                new object[] { 3,4},
+                new object[] { 5,6}
+            };
+        }
+
+        public static IEnumerable<object[]> GetNumbers2
+        {
+            get
+            {
+                return new List<object[]>
+                {
+                    new object[] { 1,2},
+                    new object[] { 3,4},
+                    new object[] { 5,6}
+                };
+
+            }
+        }
+
+
+
     }
 }
