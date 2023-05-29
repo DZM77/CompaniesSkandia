8667d941b98275e862438c7ab7d00d493fd85395
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-05-29 12:31:54 +0200

    Test with Func

---
+++ Companies.API/Services/ExtensionServiceDemo.cs
@@ -0,0 +1,51 @@
+﻿namespace Companies.API.Services
+{
+    public class ExtensionServiceDemo : IExtensionServiceDemo
+    {
+        private IConfiguration configuration { get; }
+        public ExtensionServiceDemo(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string DemoMethod()
+        {
+            var value = configuration.GetValueFromExtension("SomeValue");
+            //Do work
+
+            return value.ToString();
+        }
+
+    }
+
+    public interface IExtensionServiceDemo
+    {
+        string DemoMethod();
+    }
+
+    public static class ConfigExtensions
+    {
+        public static Func<IConfiguration, string, string> Implementation { private get; set; } = (config, key) => config.GetValue<string>(key)!;
+        public static string GetValueFromExtension(this IConfiguration configuration, string key)
+        {
+            return Implementation(configuration, key);
+        }
+    } 
+    
+    //public class GetValueFromConfig : IGetValueFromConfig
+    //{
+    //    public  string GetValue(IConfiguration configuration, string key)
+    //    {
+    //        return configuration.GetValue<string>(key)!;
+    //    }
+    //}
+
+    //public interface IGetValueFromConfig
+    //{
+    //    string GetValue(IConfiguration configuration, string key);
+    //}
+
+
+
+
+}
---
+++ Employees.Tests/Services/ExtensionDemoTests.cs
@@ -0,0 +1,34 @@
+﻿using Companies.API.Services;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Tests.Services
+{
+    public class ExtensionDemoTests
+    {
+
+        [Fact]
+        public void DemoMethod_ShouldReturnValue()
+        {
+            const string expected = "from config";
+            var iconfig = new Mock<IConfiguration>();
+            var sut = new ExtensionServiceDemo(iconfig.Object);
+
+            ConfigExtensions.Implementation = (c, k) => expected;
+
+            var actual = sut.DemoMethod();
+
+            Assert.Equal(expected, actual);
+        }
+
+        //public string DemoMethod(IConfiguration c, string k) 
+        //{
+        //    return "from config";
+        //}
+    }
+}
