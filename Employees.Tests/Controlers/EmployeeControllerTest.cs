dda4cefc9ec7a1cf1e2fda0473aeef59734cd9ce
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-05-31 09:52:01 +0200

    EmployeeController tests - not working :)

---
+++ Employees.Tests/Controlers/EmployeeControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using Companies.API.Controllers;
+using Companies.API.Data;
+using Employees.Tests.Fixtures;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Tests.Controlers
+{
+    public class EmployeeControllerTest : IClassFixture<DataBaseFixture>
+    {
+        private readonly DataBaseFixture fixture;
+        private ApplicationDbContext context;
+
+        public EmployeeControllerTest(DataBaseFixture fixture)
+        {
+            context = fixture.Context;
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public async Task GetEmployeesForCompany_CompanyDontExist_ShouldReturnNotFound()
+        {
+            var notExistingId = new Guid();
+            var sut = new EmplyeesController(context, fixture.Mapper);
+
+            var res = await sut.GetEmployeesForCompany(notExistingId, null);
+
+            Assert.NotNull(res);
+            Assert.IsType<NotFoundResult>(res);
+        }
+
+        [Fact]
+        public async Task GetEmployeesForCompany_CompanyExist_ShouldReturnOk()
+        {
+            var expectedCompanyId = context.Companies.First().Id;
+            var sut = new EmplyeesController(context, fixture.Mapper);
+
+            var res = await sut.GetEmployeesForCompany(expectedCompanyId, null);
+
+            Assert.NotNull(res);
+            Assert.IsType<OkObjectResult>(res);
+        }
+    }
+}
---
+++ Employees.Tests/Fixtures/DataBaseFixture.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Companies.API;
+using Companies.API.Data;
+using Companies.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Tests.Fixtures
+{
+    public class DataBaseFixture : IDisposable
+    {
+        public Mapper Mapper { get; }
+        public ApplicationDbContext Context { get; }
+
+        public DataBaseFixture()
+        {
+            Mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MapperProfile>();
+            }));
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+               .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TestDataBase;Trusted_Connection=True;MultipleActiveResultSets=true")
+               .Options;
+
+            var context = new ApplicationDbContext(options);
+
+            context.Database.Migrate();
+
+            context.Companies.AddRange(new Company[]
+                     {
+                           new Company()
+                           {
+                               Name = "TestCompanyName",
+                               Address = "TestAdress",
+                               Country = "TestCountry",
+                               Employees = new User[]
+                               {
+                                   new User
+                                   {
+                                       UserName = "TestUserName",
+                                       Email = "test@test.com",
+                                       Age = 50,
+                                       Name = "TestName",
+                                       Position = "TestPosition"
+                                   }
+                               }
+                           }
+                     });
+
+            context.SaveChanges();
+            Context = context;
+
+        }
+
+        public void Dispose()
+        {
+            Context.Database.EnsureDeleted();
+            Context.Dispose();
+        }
+    }
+}
