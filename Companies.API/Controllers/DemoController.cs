6d4d9c8be45e0bcac21bd39c279d9faf5ab1fe15
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-06-01 10:56:19 +0200

    Add some IntegrationTests

--- Companies.API/Controllers/DemoController.cs
+++ Companies.API/Controllers/DemoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Companies.API.DataTransferObjects;
+using Companies.API.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,11 +10,43 @@ namespace Companies.API.Controllers
     [ApiController]
     public class DemoController : ControllerBase
     {
+        private readonly IServiceManager serviceManager;
+
+        public DemoController(IServiceManager serviceManager)
+        {
+            this.serviceManager = serviceManager;
+        }
+
         [HttpGet]
         [AllowAnonymous]
         public ActionResult Index()
         {
             return Ok("Working");
         }
+
+        [HttpGet("dto")]
+        [AllowAnonymous]
+        public ActionResult Index2()
+        {
+            var dto = new CompanyDto { Name = "Working" };
+            return Ok(dto);
+        }
+
+        [HttpGet("getonefromservice")]
+        [AllowAnonymous]
+        public async Task<ActionResult> Get()
+        {
+            var dto = (await serviceManager.CompanyService.GetCompaniesAsync(true)).First();
+            return Ok(dto);
+        }
+
+        [HttpGet("getall")]
+        [AllowAnonymous]
+        public async Task<ActionResult> GetAll()
+        {
+            var dtos = await serviceManager.CompanyService.GetCompaniesAsync(false);
+            return Ok(dtos);
+        }
+
     }
 }
--- Companies.Integration.Tests/DemoTests.cs
+++ Companies.Integration.Tests/DemoTests.cs
@@ -1,24 +1,76 @@
 using Companies.API;
+using Companies.API.DataTransferObjects;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
+using System.Text.Json;
 
 namespace Companies.Integration.Tests
 {
     public class DemoTests : IClassFixture<WebApplicationFactory<Program>>
     {
-        private HttpClient httpClient;
+        private readonly HttpClient httpClient;
 
         public DemoTests(WebApplicationFactory<Program> applicationFactory)
         {
-                httpClient = applicationFactory.CreateDefaultClient();
+            applicationFactory.ClientOptions.BaseAddress = new Uri("https://localhost:7019/api/demo/");
+            httpClient = applicationFactory.CreateClient();
         }
 
         [Fact]
         public async Task Index_ShouldReturnOk()
         {
-            var response = await httpClient.GetAsync("api/demo");
-
+            var response = await httpClient.GetAsync("");
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+        [Fact]
+        public async Task Index_ShouldReturnExpectedMessage()
+        {
+            var response = await httpClient.GetAsync("");
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal("Working", content);
+            Assert.Equal("text/plain", response.Content.Headers.ContentType.MediaType);
+
+        }
+
+        [Fact]
+        public async Task Index2_ShouldReturnExpectedMediaType()
+        {
+            var response = await httpClient.GetAsync("dto");
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var dto = JsonSerializer.Deserialize<CompanyDto>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            Assert.Equal("Working", dto.Name);
+            Assert.Equal("application/json", response.Content.Headers.ContentType.MediaType);
+
+        }
+
+        [Fact]
+        public async Task Index3_ShouldReturnExpectedMessage_WithStream()
+        {
+            var response = await httpClient.GetStreamAsync("dto");
+
+            var dto = await JsonSerializer.DeserializeAsync<CompanyDto>(response, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            Assert.Equal("Working", dto.Name);
+
+        }
+
+        [Fact]
+        public async Task Index4_ShouldReturnExpectedMessageSimplifyed()
+        {
+            var dto = await httpClient.GetFromJsonAsync<CompanyDto>("dto");
+            Assert.Equal("Working", dto.Name);
+
         }
     }
 }
